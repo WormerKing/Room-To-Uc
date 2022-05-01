87bc3fb5521edc58a4b880c8ee4e1a354b3255ce
WormerKing <aferinsanaaferin@gmail.com>
2022-05-01 15:44:18 +0300

    Fixed ActionMailer production environment bugs

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'roomtouc@gmail.com'
+  default from: 'shinyuna648@gmail.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require_relative "application"
 
+ActionMailer::Base.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    user_name: 'syuna648@gmail.com',
+    password: Base64.decode64("U2hpbll1bmEwOTkw\n"),
+    authentication: :plain,
+    enable_starttls_auto: true,
+    domain:'gmail.com',
+    openssl_verify_mode: 'none'
+  }
+
 # Initialize the Rails application.
 Rails.application.initialize!
